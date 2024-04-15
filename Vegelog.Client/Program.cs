ba7b346f4c7a5f6fcce484a1a47ca9a0bd824b2a
fukicycle <lego.sato.4135@gmail.com>
2024-04-15 10:46:32 +0900

    initial commit

---
+++ Vegelog.Client/Program.cs
@@ -0,0 +1,13 @@
+using Fukicycle.Tool.AppBase;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Vegelog.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("Default", sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ServiceURI"] + "api/v1/") });
+builder.Services.AddAppBase();
+
+await builder.Build().RunAsync();
