6df7525f5422a3253bd9b78ce9ec39a1f8224469
fukicycle <lego.sato.4135@gmail.com>
2024-04-15 19:12:14 +0900

    add upload image(thumbnail)

--- Vegelog.Client/Pages/Diary.razor.cs
+++ Vegelog.Client/Pages/Diary.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Vegelog.Client.Pages
 {
@@ -10,10 +11,34 @@ namespace Vegelog.Client.Pages
         [Parameter, SupplyParameterFromQuery(Name = "id")]
         public Guid Id { get; set; }
 
+        private string? _thumbnail = null;
+
         protected override void OnInitialized()
         {
             base.OnInitialized();
             Content = Id.ToString();
+        }
+
+        private async Task FileOnChanged(InputFileChangeEventArgs e)
+        {
+            await GetThumbnail(e.File);
+        }
+
+        private async Task GetThumbnail(IBrowserFile file)
+        {
+            string extensionName = Path.GetExtension(file.Name);
+
+            List<string> imageFileTypes = new List<string> { ".png", ".jpg", ".jpeg" };
+            if (imageFileTypes.Contains(extensionName))
+            {
+                var resizedFile = await file.RequestImageFileAsync(file.ContentType, 1280, 720);
+                var buf = new byte[resizedFile.Size];
+                using (var stream = resizedFile.OpenReadStream())
+                {
+                    await stream.ReadAsync(buf);
+                }
+                _thumbnail = "data:image/png;base64," + Convert.ToBase64String(buf);
+            }
         }
     }
 }
