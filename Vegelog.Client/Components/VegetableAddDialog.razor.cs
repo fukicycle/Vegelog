f29cd975acdc9e59fef181908dd559eb83f4614a
fukicycle <lego.sato.4135@gmail.com>
2024-04-15 17:07:54 +0900

    implments add dialog

---
+++ Vegelog.Client/Components/VegetableAddDialog.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Vegelog.Shared.Dto.Response;
+
+namespace Vegelog.Client.Components
+{
+    public partial class VegetableAddDialog
+    {
+        [Parameter]
+        public EventCallback<MouseEventArgs> CancelButtonOnClick { get; set; }
+
+        [Parameter]
+        public EventCallback<Vegetable> OkButtonOnClick { get; set; }
+
+        private string Name { get; set; } = string.Empty;
+        private string? Description { get; set; } = null;
+
+        private async Task VegetableAddButtonOnClick()
+        {
+            Vegetable vegetable = new Vegetable(Guid.Empty, Name, Description, null);
+            await OkButtonOnClick.InvokeAsync(vegetable);
+        }
+    }
+}
--- Vegelog.Client/Pages/Home.razor.cs
+++ Vegelog.Client/Pages/Home.razor.cs
@@ -5,6 +5,7 @@ namespace Vegelog.Client.Pages
     public partial class Home
     {
         private List<Vegetable> _vegetables = new List<Vegetable>();
+        private bool _isDialogOpen = false;
 
         protected override void OnInitialized()
         {
@@ -12,6 +13,22 @@ namespace Vegelog.Client.Pages
             _vegetables.Add(new Vegetable(Guid.Empty, "ピーマン", "おいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはず", null));
             _vegetables.Add(new Vegetable(Guid.Empty, "ピーマン", "おいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはず", null));
             _vegetables.Add(new Vegetable(Guid.Empty, "ピーマン", "おいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはずおいしいピーマンに育ってくれるはず", null));
+        }
+
+        private void AddButtonOnClick()
+        {
+            _isDialogOpen = true;
+        }
+
+        private void CancelButtonOnClick()
+        {
+            _isDialogOpen = false;
+        }
+
+        private void OkButtonOnClick(Vegetable vegetable)
+        {
+            _vegetables.Add(vegetable);
+            _isDialogOpen = false;
         }
     }
 }
