3e17e936b3460b5ce465e97bfa3d4835ffc0f3d5
fukicycle <lego.sato.4135@gmail.com>
2024-04-16 11:09:12 +0900

    register service

--- Vegelog.Client/Program.cs
+++ Vegelog.Client/Program.cs
@@ -2,6 +2,7 @@ using Fukicycle.Tool.AppBase;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Vegelog.Client;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("Default", sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ServiceURI"] + "api/v1/") });
 builder.Services.AddAppBase();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
