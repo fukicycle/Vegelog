75b41613940f23cc7038ff7e3657173d6af255da
fukicycle <lego.sato.4135@gmail.com>
2024-04-16 10:45:28 +0900

    Add group reponse dto

---
+++ Vegelog.Shared/Dto/Response/GroupResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Vegelog.Shared.Dto.Response
+{
+    public sealed class GroupResponseDto
+    {
+        public GroupResponseDto(Guid id, string code, List<VegetableResponseDto> vegetables)
+        {
+            Id = id;
+            Code = code;
+            Vegetables = vegetables;
+        }
+
+        public Guid Id { get; }
+        public string Code { get; }
+        public List<VegetableResponseDto> Vegetables { get; }
+    }
+}
